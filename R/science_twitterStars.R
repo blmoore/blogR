2c70879ebb96ef9f7f78711fabbac00dc37b528f
Ben Moore <b3nmoore@gmail.com>
2014-09-19 13:18:07 +0100

    plot figshare dataset too

--- R/science_twitterStars.R
+++ R/science_twitterStars.R
@@ -10,5 +10,13 @@ i$xAxis(type = "addLogAxis", overrideMin=10)
 # log
 i
 
-
 i$publish("The top 50 science stars of Twitter", host="gist")
+
+f <- read.csv2("http://files.figshare.com/1679269/top_tweeters.txt", sep="\t")
+
+i2 <- dPlot(Papers ~ Retweets, data=f, groups="Account",
+      height=500, width=500)
+i2$yAxis(type = "addLogAxis", overrideMin=1)
+i2$xAxis(type = "addLogAxis", overrideMin=500)
+i2
+
